834aa75d7c7e5f8fd2316e28b8d60a5ec4d8eef2
dyfca <dyfca@icloud.com>
2018-04-28 21:41:43 -0500

    part 3

--- TCP_UDP_Proxy.py
+++ TCP_UDP_Proxy.py
@@ -2,35 +2,29 @@ from socket import *
 def ReceiveQuery():
     serverPort_UDP = 53
     serverSocket_UDP = socket(AF_INET, SOCK_DGRAM)
-    serverSocket_UDP.bind(('',serverPort_UDP))
+    serverSocket_UDP.bind(('', serverPort_UDP))
 
     serverPort_TCP = 53
     serverSocket_TCP = socket(AF_INET, SOCK_STREAM)
     serverSocket_TCP.bind(('', serverPort_TCP))
     serverSocket_TCP.listen(1)
 
-    print( "The server is ready to receive")
+    print("The server is ready to receive")
     while True:
 
         message, clientAddress = serverSocket_UDP.recvfrom(2048)
         response, trunc = UDP_SendQueryToResolver(message)
-        print "trunc" + str(trunc)
         serverSocket_UDP.sendto(response, clientAddress)
-        if trunc == True:
-            # serverPort_TCP = 53
-            # serverSocket_TCP = socket(AF_INET,SOCK_STREAM)
-            # serverSocket_TCP.bind(('',serverPort_TCP))
-            # serverSocket_TCP.listen(1)
+        # print "trunc" + str(trunc)
+        if trunc:
+            connectionSocket, addr = serverSocket_TCP.accept()
             print 'The TCP server is ready to receive'
-            connectionSocket,addr = serverSocket_TCP.accept()
-            print str(connectionSocket)
+            # print str(connectionSocket)
             message = connectionSocket.recv(2048)
             response = TCP_SendQueryToResolver(message)
-            print response
+            print len(response)
             connectionSocket.send(response)
             connectionSocket.close()
-
-
 
 
 def UDP_SendQueryToResolver(message):
@@ -48,16 +42,21 @@ def UDP_SendQueryToResolver(message):
     clientSocket.close()
     return response, trunc
 
+
 def TCP_SendQueryToResolver(message):
     serverName = "8.8.8.8"
     serverPort = 53
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((serverName, serverPort))
     clientSocket.send(message)
-    response = clientSocket.recv(2048)
+    response = ''
+    while True:
+        res = clientSocket.recv(2048)
+        if not res:
+            break
+        response += res
     clientSocket.close()
     return response
-
 
 
 if __name__ == "__main__":
